d16875dd11c425884005228e36406c90320f83cd
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-10-31 23:04:20 -0300

    added validation to empty string

--- src/mongoDB/clientSchema.js
+++ src/mongoDB/clientSchema.js
@@ -3,26 +3,46 @@ const mongoose = require('mongoose');
 const clientSchema = new mongoose.Schema({
   nombre: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: (value) => value.trim().length > 0, // Valida que no esté vacío
+      message: 'El campo "nombre" no puede estar vacío.'
+    }
   },
   direccion: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: (value) => value.trim().length > 0, // Valida que no esté vacío
+      message: 'El campo "direccion" no puede estar vacío.'
+    }
   },
   direccion2: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: (value) => value.trim().length > 0, // Valida que no esté vacío
+      message: 'El campo "direccion2" no puede estar vacío.'
+    }
   },
   country: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: (value) => value.trim().length > 0, // Valida que no esté vacío
+      message: 'El campo "country" no puede estar vacío.'
+    }
   },
   vatNumber: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: (value) => value.trim().length > 0, // Valida que no esté vacío
+      message: 'El campo "vatNumber" no puede estar vacío.'
+    }
   },
-  emoji:{
-    type:String
+  emoji: {
+    type: String
   }
 });
 
